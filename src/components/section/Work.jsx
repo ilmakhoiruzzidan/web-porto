663044cdf621fd45d10b1894483a15c789e52fa1
ilmakhoiruzzidan <58930922+ilmakhoiruzzidan@users.noreply.github.com>
2025-11-08 19:51:32 +0700

    fix some link for placehorders

--- src/components/section/Work.jsx
+++ src/components/section/Work.jsx
@@ -10,19 +10,16 @@ function Work() {
                 <p className='text-4xl font-bold inline border-b-2 border-[#333333]'>Work</p>
                 <p className='py-6'>// Check out some of my recent Projects</p>
             </div>
-            {/* Container */}
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8'>
                
-                {/* Grid Item */}
                 <div style={{backgroundImage: `url(${WorkImg})`}} className='shadow-lg group container rounded-md flex justify-center items-center mx-auto content-div'>
                     
-                    {/* Hover Effects */}
                     <div className='opacity-0 group-hover:opacity-100 text-center'>
                         <span className='text-2xl font-bold text-white tracking-wider'>
                             Belanja Skuy
                         </span>
                         <div className='pt-8 text-center'>
-                            <a href="">
+                            <a href="#">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg' style={{ display :'none' }}>Demo</button>
                             </a>
                             <a href="https://github.com/ilmakhoiruzzidan/Belanja-Skuy" target='blank_'>
@@ -34,7 +31,6 @@ function Work() {
                 </div>
                 <div style={{backgroundImage: `url(${Project2Img})`}} className='shadow-lg group container rounded-md flex justify-center items-center mx-auto content-div'>
                     
-                    {/* Hover Effects */}
                     <div className='opacity-0 group-hover:opacity-100 text-center'>
                         <span className='text-xl font-semibold text-white tracking-wider'>
                             Image Recognition using TensorFlow
@@ -52,7 +48,6 @@ function Work() {
                 </div>
                 <div style={{backgroundImage: `url(${WorkImg})`}} className='shadow-lg group container rounded-md flex justify-center items-center mx-auto content-div'>
                     
-                    {/* Hover Effects */}
                     <div className='opacity-0 group-hover:opacity-100 text-center'>
                         <span className='text-2xl font-bold text-white tracking-wider'>
                             Placeholder
